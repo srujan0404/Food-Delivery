30b8ce8b5335c03e2cdf62cef4a04d6678ef4cdd
srujan0404 <srujan.23bcs10107@ms.sst.scaler.com>
2024-07-11 22:26:23 +0530

    added two more screens

--- client/components/basketIcon.js
+++ client/components/basketIcon.js
@@ -1,11 +1,11 @@
-import { View, Text, TouchableOpacity } from 'react-native';
-import React, { useMemo } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectBasketItems, selectBasketTotal } from '../slices/basketSlice';
-import { useNavigation } from '@react-navigation/native';
-import { selectResturant } from '../slices/resturantSlice';
-import { themeColors } from '../theme';
-import PropTypes from 'prop-types';
+import { View, Text, TouchableOpacity } from "react-native";
+import React, { useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectBasketItems, selectBasketTotal } from "../slices/basketSlice";
+import { useNavigation } from "@react-navigation/native";
+import { selectResturant } from "../slices/resturantSlice";
+import { themeColors } from "../theme";
+import PropTypes from "prop-types";
 
 const BasketIcon = () => {
   const basketItems = useSelector(selectBasketItems);
@@ -21,15 +21,22 @@ const BasketIcon = () => {
     <View className="absolute bottom-5 w-full z-50">
       <TouchableOpacity
         style={{ backgroundColor: themeColors.bgColor(1) }}
-        onPress={() => navigation.navigate('Cart')}
+        onPress={() => navigation.navigate("Cart")}
         className="flex-row justify-between items-center mx-5 rounded-full p-4 py-3 shadow-lg"
       >
-        <View className="p-2 px-4 rounded-full" style={{ backgroundColor: 'rgba(255,255,255,0.3)' }}>
+        <View
+          className="p-2 px-4 rounded-full"
+          style={{ backgroundColor: "rgba(255,255,255,0.3)" }}
+        >
           <Text className="font-extrabold text-white text-lg">{itemCount}</Text>
         </View>
 
-        <Text className="flex-1 text-center font-extrabold text-white text-lg">View Cart</Text>
-        <Text className="font-extrabold text-white text-lg">${totalAmount}</Text>
+        <Text className="flex-1 text-center font-extrabold text-white text-lg">
+          View Cart
+        </Text>
+        <Text className="font-extrabold text-white text-lg">
+          ${totalAmount}
+        </Text>
       </TouchableOpacity>
     </View>
   );
