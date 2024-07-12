e183bcde46474db70475be655bf70347f4086f0d
srujan0404 <srujan.23bcs10107@ms.sst.scaler.com>
2024-07-12 11:21:22 +0530

    added slices

--- client/screens/ResturantScreen.js
+++ client/screens/ResturantScreen.js
@@ -1,111 +1,133 @@
-import { View, Text, StatusBar, ScrollView, Image, TouchableOpacity } from 'react-native'
-import React, { useEffect, useLayoutEffect } from 'react'
-import { useNavigation, useRoute } from '@react-navigation/native'
-import { urlFor } from '../sanity';
-import DishRow from '../components/dishRow';
-import BasketIcon from '../components/basketIcon';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectResturant, setResturant } from '../slices/resturantSlice';
-import { emptyBasket } from '../slices/basketSlice';
+import {
+  View,
+  Text,
+  StatusBar,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import React, { useEffect, useLayoutEffect } from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { urlFor } from "../sanity";
+import DishRow from "../components/dishRow";
+import BasketIcon from "../components/BasketIcon";
+import { useDispatch, useSelector } from "react-redux";
+import { selectResturant, setResturant } from "../slices/resturantSlice";
+import { emptyBasket } from "../slices/basketSlice";
 import * as Icon from "react-native-feather";
-import { themeColors } from '../theme';
+import { themeColors } from "../theme";
 
 export default function ResturantScreen() {
-    const navigation = useNavigation();
-    const resturant = useSelector(selectResturant);
-    let dispatch = useDispatch();
-    const {params: {
-        id, 
+  const navigation = useNavigation();
+  const resturant = useSelector(selectResturant);
+  const dispatch = useDispatch();
+
+  const {
+    params: {
+      id,
+      title,
+      imgUrl,
+      rating,
+      type,
+      address,
+      description,
+      dishes,
+      lng,
+      lat,
+    },
+  } = useRoute();
+
+  // Set navigation options to hide the header
+  useLayoutEffect(() => {
+    navigation.setOptions({ headerShown: false });
+  }, []);
+
+  // Effect to handle restaurant data and basket state
+  useEffect(() => {
+    if (resturant && resturant.id !== id) {
+      dispatch(emptyBasket());
+    }
+    dispatch(
+      setResturant({
+        id,
         title,
         imgUrl,
         rating,
         type,
-        address, 
+        address,
         description,
         dishes,
         lng,
-        lat
-    }} = useRoute();
-    useLayoutEffect(()=>{
-        navigation.setOptions({headerShown: false})
-    },[])
-    useEffect(()=>{
-        if(resturant && resturant.id!=id){
-            dispatch(emptyBasket());
-        }
-        dispatch(setResturant({
-            id, 
-            title,
-            imgUrl,
-            rating,
-            type,
-            address, 
-            description,
-            dishes,
-            lng,
-            lat
-        }))
-    },[])
+        lat,
+      })
+    );
+  }, [resturant, id, dispatch]);
+
   return (
     <>
-        <BasketIcon />
-        <ScrollView  >
-            <View className="relative">
-                <Image className="w-full h-72" source={{uri: urlFor(imgUrl).url()}} />
-                <TouchableOpacity 
-                    onPress={()=>navigation.goBack()} 
-                    className="absolute top-14 left-4 bg-gray-50 p-2 rounded-full shadow">
-                    <Icon.ArrowLeft strokeWidth={3} stroke={themeColors.bgColor(1)} />
-                </TouchableOpacity>
+      <BasketIcon />
+      <ScrollView>
+        {/* Restaurant Image and Back Button */}
+        <View className="relative">
+          <Image
+            className="w-full h-72"
+            source={{ uri: urlFor(imgUrl).url() }}
+          />
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            className="absolute top-14 left-4 bg-gray-50 p-2 rounded-full shadow"
+          >
+            <Icon.ArrowLeft strokeWidth={3} stroke={themeColors.bgColor(1)} />
+          </TouchableOpacity>
+        </View>
+
+        {/* Restaurant Info Section */}
+        <View
+          style={{ borderTopLeftRadius: 40, borderTopRightRadius: 40 }}
+          className="bg-white -mt-12 pt-6"
+        >
+          <View className="px-5">
+            <Text className="text-3xl font-bold">{title}</Text>
+            {/* Restaurant Rating and Address */}
+            <View className="flex-row space-x-2 my-1">
+              <View className="flex-row items-center space-x-1">
+                <Image
+                  source={require("../assets/images/fullStar.png")}
+                  className="h-4 w-4"
+                />
+                <Text className="text-xs">
+                  <Text className="text-green-700">{rating}</Text>
+                  <Text className="text-gray-700"> (4.6k review)</Text> ·{" "}
+                  <Text className="font-semibold text-gray-700">{type}</Text>
+                </Text>
+              </View>
+              <View className="flex-row items-center space-x-1">
+                <Icon.MapPin color="gray" width={15} height={15} />
+                <Text className="text-gray-800 text-xs">
+                  {" "}
+                  Nearby · {address}
+                </Text>
+              </View>
             </View>
-            <View 
-                style={{borderTopLeftRadius: 40, borderTopRightRadius: 40}} 
-                className="bg-white -mt-12 pt-6">
-                <View className="px-5">
-                    <Text className="text-3xl font-bold">{title}</Text>
-                    {/* copy this code from restaurant card */}
-                    <View className="flex-row space-x-2 my-1">
-                        <View className="flex-row items-center space-x-1">
-                            <Image 
-                                source={require('../assets/images/fullStar.png')} 
-                                className="h-4 w-4" />
-                            <Text className="text-xs">
-                                <Text className="text-green-700">{rating}</Text>
-                                <Text className="text-gray-700"> (4.6k review)</Text> · <Text className="font-semibold text-gray-700">{type}</Text>
-                            </Text>
-                        </View>
-                        <View className="flex-row items-center space-x-1">
-                            <Icon.MapPin color="gray" width={15} height={15} />
-                            <Text className="text-gray-800 text-xs"> Nearby · {address}</Text>
-                        </View>
-                    </View>
-                    <Text className="text-gray-500 mt-2">{description}</Text>
-                    
-                    
-                </View>
-                
-            </View>
-            <View className="pb-36 bg-white">
-                <Text className="px-4 py-4 text-2xl font-bold">Menu</Text>
-                {/* dishes */}
-                {
-                    dishes.map(dish=>{
-                        return (
-                            <DishRow 
-                                key={dish._id}
-                                id={dish._id}
-                                name={dish.name}
-                                description={dish.description}
-                                price={dish.price}
-                                image={dish.image}
-                            />
-                        )
-                    })
-                }
-            </View>
-      
-        </ScrollView>
+            <Text className="text-gray-500 mt-2">{description}</Text>
+          </View>
+        </View>
+
+        {/* Menu Section */}
+        <View className="pb-36 bg-white">
+          <Text className="px-4 py-4 text-2xl font-bold">Menu</Text>
+          {dishes.map((dish) => (
+            <DishRow
+              key={dish._id}
+              id={dish._id}
+              name={dish.name}
+              description={dish.description}
+              price={dish.price}
+              image={dish.image}
+            />
+          ))}
+        </View>
+      </ScrollView>
     </>
-    
-  )
+  );
 }
